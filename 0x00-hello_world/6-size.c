62b1ea55b90cb8589bccc61ea1cb3475f9421658
Zaakaria <zikolbyad1@gmail.com>
2023-06-15 07:56:55 -0700

    Low Level Programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("Size of char: %d bytes\n", sizeof(char));
+    printf("Size of int: %d bytes\n", sizeof(int));
+    printf("Size of long int: %d bytes\n", sizeof(long));
+    printf("Size of long long: %d bytes\n", sizeof(long long));
+    printf("Size of float: %d bytes\n", sizeof(float));
+    return 0;
+}
