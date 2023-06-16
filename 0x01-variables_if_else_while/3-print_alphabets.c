35de8f9c090f388ec656e8b91d6d98c45d7b1c92
Zaakaria <zikolbyad1@gmail.com>
2023-06-16 09:21:45 -0700

    C Variables, If Else, While

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-/**
- * main - prints the alphabet in lowercase, and then in uppercase
- *
- * Return: 0
- */
 
-int main(void){
-char str[52] = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-int i = 0;
-while (str[i] != "Z"){
-	putchar(str[i]);
-	i++;
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
-putchar("\n");
-return 0;
-}
+
