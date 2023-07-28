97834bd9489791fa7e9a41ce610eba07c458c2c3
Zaakaria <zikolbyad1@gmail.com>
2023-07-28 12:51:21 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,6 +19,6 @@ size_t list_len(const list_t *p);
 list_t *add_node(list_t **d, const char *str);
 size_t print_list(const list_t *p);
 list_t *add_node_end(list_t **d, const char *str);
-void free_list(list_t *head);
+void free_list(list_t *d);
 
 #endif
