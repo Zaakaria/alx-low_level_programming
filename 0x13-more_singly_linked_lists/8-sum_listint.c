deba0ffd66deace02481e653ff0045bf1c30ed3a
Zaakaria <zikolbyad1@gmail.com>
2023-08-01 12:27:56 -0700

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - it will calculate the total sum of all the data in the listint_t list
+ * sum_listint - it will calculate the total of data in listint_t list
  * @head: it is the linked list's first node
  *
  * Return: the whole sum 
