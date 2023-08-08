330d25067f0f3122c7f3eac47c96181c25276fc9
Zaakaria <zikolbyad1@gmail.com>
2023-08-07 17:52:34 -0700

    C File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
  * @filename: it is a pointer to the file's name 
  * @text_content: it is string to be add to file's end 
  *
- * Return: -1 if fails, filename NULL, file not exist 
+ * Return: -1 if it fails, 1 otherwise 
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
